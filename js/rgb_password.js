c7e3a555e95cb5c4e2c40a28b2ccd2206392d2f6
Adam Howard <adz@skatty.me>
2012-03-07 00:26:46 +0000

    Fix indentation

--- js/rgb_password.js
+++ js/rgb_password.js
@@ -8,14 +8,14 @@
 
 // http://werxltd.com/wp/2010/05/13/javascript-implementation-of-javas-string-hashcode-method/
 String.prototype.hashCode = function(){
-	var hash = 0;
-	if (this.length == 0) return hash;
-	for (i = 0; i < this.length; i++) {
-		char = this.charCodeAt(i);
-		hash = ((hash<<5)-hash)+char;
-		hash = hash & hash; // Convert to 32bit integer
-	}
-	return hash;
+  var hash = 0;
+  if (this.length == 0) return hash;
+  for (i = 0; i < this.length; i++) {
+    char = this.charCodeAt(i);
+    hash = ((hash<<5)-hash)+char;
+    hash = hash & hash; // Convert to 32bit integer
+  }
+  return hash;
 }
 
 // Based on http://mjijackson.com/2008/02/rgb-to-hsl-and-rgb-to-hsv-color-model-conversion-algorithms-in-javascript
@@ -49,37 +49,37 @@ function hslToRgb(h, s, l){
 
 
 window.onload = function() {
-	var password = {
-		selector: document.getElementById("password"),
-		boxes: [
-		  document.getElementById("one"),
+  var password = {
+    selector: document.getElementById("password"),
+    boxes: [
+      document.getElementById("one"),
       document.getElementById("two"),
       document.getElementById("three")
-		],
-		saturation: 0.5,
-		lightness: 0.5,
-		salts: [
-		  2462,
-		  3637,
-		  7432
-		]
-	};
-	
-	password.selector.onkeyup = function() {
-	  var hashCode = password.selector.value.hashCode();
-		
-		for(var i in password.boxes) {
-		  var box = password.boxes[i];
+    ],
+    saturation: 0.5,
+    lightness: 0.5,
+    salts: [
+      2462,
+      3637,
+      7432
+    ]
+  };
+  
+  password.selector.onkeyup = function() {
+    var hashCode = password.selector.value.hashCode();
+    
+    for(var i in password.boxes) {
+      var box = password.boxes[i];
 
       var salt = password.salts[i];
       var hue = (hashCode % salt)/salt;
       var color = hslToRgb(hue, password.saturation, password.lightness);
       
-  		box.style.backgroundColor = "rgb("
-  			+ color.r + ", "
-  			+ color.g + ", "
-  			+ color.b +
-  		")";
-  	}
-	}
+      box.style.backgroundColor = "rgb("
+        + color.r + ", "
+        + color.g + ", "
+        + color.b +
+      ")";
+    }
+  }
 }
